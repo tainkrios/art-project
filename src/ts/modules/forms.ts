bcaaf322773546bd4b41d80b81e219125546d375
Ivan Zhdanov <tain.krios@gmail.com>
2022-05-17 23:05:33 +0300

    json

--- src/ts/modules/forms.ts
+++ src/ts/modules/forms.ts
@@ -1,3 +1,5 @@
+import { postData } from "../services/requests";
+
 export const forms = () => {
   const forms = document.querySelectorAll('form'),
         inputs = document.querySelectorAll('input'),
@@ -15,14 +17,6 @@ export const forms = () => {
   const path = {
     designer: 'assets/server.php',
     question: 'assets/question.php'
-  }
-
-  const postData = async (url: string, data: FormData) => {
-    const res = await fetch(url, {
-      method: 'POST',
-      body: data
-    })
-    return await res.text()
   }
 
   const clearInputs = () => {
--- src/ts/modules/showMoreStyles.ts
+++ src/ts/modules/showMoreStyles.ts
@@ -1,17 +1,30 @@
-export const showMoreStyles = (trigger: string, styles: string) => {
-  const cards = document.querySelectorAll(styles),
-        btn = document.querySelector(trigger)
+import { getResource } from "../services/requests";
 
-  cards.forEach(card => {
-    card.classList.add('animated', 'fadeInUp')
+export const showMoreStyles = (trigger: string, wrapper: string) => {
+  const btn = document.querySelector(trigger)
+
+  btn.addEventListener('click', function() {
+    getResource('assets/db.json')
+      .then(res => createCards(res.styles))
+      .catch(error => console.log(error))
+
+    this.remove()
   })
 
-  btn.addEventListener('click', () => {
-    cards.forEach(card => {
-      card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs')
-      card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1')
-    })
-    
-    btn.remove()
-  })
+  const createCards = (response: {src: string, title: string, link: string}[]) => {
+    response.forEach(({src, title, link}) => {
+      let card = document.createElement('div')
+      
+      card.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1')
+      
+      card.innerHTML = `
+        <div class='styles-block'>
+          <img src=${src} alt='style'>
+          <h4>${title}</h4>
+          <a href="${link}">Подробнее</a>
+        </div>
+      `
+      document.querySelector(wrapper).append(card)
+    });
+  }
 }
