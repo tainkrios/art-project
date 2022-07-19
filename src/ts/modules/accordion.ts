a7795075669ec9a5283b17b7a663e10d0724b882
Ivan Zhdanov <tain.krios@gmail.com>
2022-07-19 14:22:23 +0200

    accordion with css

---
+++ src/ts/modules/accordion.ts
@@ -0,0 +1,19 @@
+export const accordion = (triggersSelector: string, itemsSelector: string) => {
+  const buttons = document.querySelectorAll(triggersSelector),
+        blocks = document.querySelectorAll(itemsSelector)
+
+  blocks.forEach(block => {
+    block.classList.add('animated', 'fadeInDown')
+  })
+
+  buttons.forEach(btn => {
+    btn.addEventListener('click', function() {
+      if (!this.classList.contains('active')) {
+        buttons.forEach(btn => {
+          btn.classList.remove('active', 'active-style')
+        })
+        this.classList.add('active', 'active-style')
+      }
+    })
+  })
+}
