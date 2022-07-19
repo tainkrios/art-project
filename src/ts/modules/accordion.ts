c138429aea5274003d9addd3182a4e5768ab4169
Ivan Zhdanov <tain.krios@gmail.com>
2022-07-19 14:42:02 +0200

    accordion JS

--- src/ts/modules/accordion.ts
+++ src/ts/modules/accordion.ts
@@ -1,19 +1,35 @@
-export const accordion = (triggersSelector: string, itemsSelector: string) => {
-  const buttons = document.querySelectorAll(triggersSelector),
-        blocks = document.querySelectorAll(itemsSelector)
-
-  blocks.forEach(block => {
-    block.classList.add('animated', 'fadeInDown')
-  })
+export const accordion = (triggersSelector: string) => {
+  const buttons = document.querySelectorAll(triggersSelector)
 
   buttons.forEach(btn => {
     btn.addEventListener('click', function() {
-      if (!this.classList.contains('active')) {
-        buttons.forEach(btn => {
-          btn.classList.remove('active', 'active-style')
-        })
-        this.classList.add('active', 'active-style')
+      this.classList.toggle('active-style')
+      this.nextElementSibling.classList.toggle('active-content')
+
+      if (this.classList.contains('active-style')) {
+        this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + 'px'
+      } else {
+        this.nextElementSibling.style.maxHeight = '0px'
       }
     })
   })
+
+
+
+        // blocks = document.querySelectorAll(itemsSelector)
+
+  // blocks.forEach(block => {
+  //   block.classList.add('animated', 'fadeInDown')
+  // })
+
+  // buttons.forEach(btn => {
+  //   btn.addEventListener('click', function() {
+  //     if (!this.classList.contains('active')) {
+  //       buttons.forEach(btn => {
+  //         btn.classList.remove('active', 'active-style')
+  //       })
+  //       this.classList.add('active', 'active-style')
+  //     }
+  //   })
+  // }) 
 }
