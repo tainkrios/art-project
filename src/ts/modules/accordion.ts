77e96201ff62592f64d955560c025e086d263055
Ivan Zhdanov <tain.krios@gmail.com>
2022-07-28 14:19:15 +0200

    fix height

--- src/ts/modules/accordion.ts
+++ src/ts/modules/accordion.ts
@@ -9,7 +9,7 @@ export const accordion = (triggersSelector: string) => {
       if (this.classList.contains('active-style')) {
         this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + 'px'
       } else {
-        this.nextElementSibling.style.maxHeight = '0px'
+        this.nextElementSibling.style.maxHeight = 0
       }
     })
   })
